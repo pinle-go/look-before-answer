fixed issues with loss and pred calculation for model2

src/main.py
@@ -107,24 +107,25 @@ class Trainer:
                 )
                 self.ema.resume(self.model)
                 self.model.train()
-                
+
                 if metrics["loss"] < best_loss:
                     best_loss = metrics["loss"]
                     no_improvement_step = 0
                 else:
                     no_improvement_step += 1
-                print(f"Best loss: {best_loss}, No improvement since: {no_improvement_step}")
+                print(
+                    f"Best loss: {best_loss}, No improvement since: {no_improvement_step}"
+                )
 
                 if no_improvement_step >= self.config.patience:
                     return np.mean(losses), True, best_loss, no_improvement_step
 
-                
         self.ema.assign(self.model)
-        self.model.eval()        
+        self.model.eval()
         metrics = evaluate(self.model, self.dev_data, self.dev_eval, self.config)
         self.ema.resume(self.model)
         self.model.train()
-                
+
         if metrics["loss"] < best_loss:
             best_loss = metrics["loss"]
             no_improvement_step = 0
@@ -159,9 +160,7 @@ class Trainer:
 
         loss = self.loss_func(p1, p2, y1, y2, z, impossibles, device=self.config.device)
         loss.backward()
-        torch.nn.utils.clip_grad_value_(
-            self.model.parameters(), self.config.grad_clip
-        )
+        torch.nn.utils.clip_grad_value_(self.model.parameters(), self.config.grad_clip)
         self.optimizer.step()
         self.scheduler.step()
         self.optimizer.zero_grad()
@@ -174,7 +173,9 @@ class Trainer:
         for i in range(self.num_epoch, self.config.max_epochs):
             self.num_epoch = i
             print(f"Training epoch {i}")
-            loss, stop, best_loss, no_improvement_step = self.train_epoch(i, best_loss, no_improvement_step)
+            loss, stop, best_loss, no_improvement_step = self.train_epoch(
+                i, best_loss, no_improvement_step
+            )
             print(f"epoch: {i}; loss : {loss}")
             if stop:
                 print(
@@ -291,7 +292,7 @@ def evaluate(model, dataset, eval_file, config):
                 ymax.tolist(),
                 version=config.version,
             )
-            
+
             answer_dict_id.update(answer_dict_id_)
             answer_dict_uuid.update(answer_dict_uuid_)
 
@@ -396,7 +397,7 @@ def test(args, config):
             Ccid = np.concatenate(
                 list(
                     map(
-                        lambda x: np.expand_dims(x["context_chars"],axis=0),
+                        lambda x: np.expand_dims(x["context_chars"], axis=0),
                         examples[i : i + config.val_batch_size],
                     )
                 ),
@@ -405,7 +406,7 @@ def test(args, config):
             Qwid = np.concatenate(
                 list(
                     map(
-                        lambda x: np.expand_dims(x["ques_tokens"],axis=0),
+                        lambda x: np.expand_dims(x["ques_tokens"], axis=0),
                         examples[i : i + config.val_batch_size],
                     )
                 ),
@@ -414,7 +415,7 @@ def test(args, config):
             Qcid = np.concatenate(
                 list(
                     map(
-                        lambda x: np.expand_dims(x["ques_chars"],axis=0),
+                        lambda x: np.expand_dims(x["ques_chars"], axis=0),
                         examples[i : i + config.val_batch_size],
                     )
                 ),
@@ -435,7 +436,7 @@ def test(args, config):
                 p1, p2 = model(Cwid, Ccid, Qwid, Qcid)
                 z = None
             ymin, ymax = pred_func(p1, p2, z)
-            
+
             if version == "v2.0":
                 for p1, p2, example in zip(
                     ymin, ymax, examples[i : i + config.val_batch_size]
@@ -445,10 +446,10 @@ def test(args, config):
                         example["span"],
                         example["context"],
                     )
-                    if (p1==-1 and p2==-1):
+                    if p1 == -1 and p2 == -1:
                         answer = ""
                     else:
-                        answer = context[span[p1][0] : span[p2][1]] 
+                        answer = context[span[p1][0] : span[p2][1]]
                     answer_dict[uuid] = answer
             else:
                 for p1, p2, example in zip(
@@ -468,8 +469,9 @@ def test(args, config):
 def main(args, config):
     if args.mode == "preprocess":
         from preprocess import preprocess
+
         try:
-            utils.makedirs('data/', raise_error=True)
+            utils.makedirs("data/", raise_error=True)
         except Exception:
             print(
                 f"Warning : data folder already exists. Some data may get overwritten"

src/models.py
@@ -572,19 +572,26 @@ class QANetV2(QANet):
         mask_C = result["mask_C"]
 
         # X1, X2 are used to calculate p1, p2
-        X1 = torch.cat([M1, M2], dim=1)
-        X2 = torch.cat([M1, M3], dim=1)
-        X3 = torch.cat([M1, M2, M3], dim=1)
-        
+        X1 = F.relu(torch.cat([M1, M2], dim=1))
+        X2 = F.relu(torch.cat([M1, M3], dim=1))
+        X3 = F.relu(torch.cat([M1, M2, M3], dim=1))
+
+        X3 = F.dropout(X3, p=self.config.dropout, training=self.training)
         p3 = mask_logits(self.z_attn_w(X3).squeeze(), mask_C)
 
         p1_ = F.softmax(p1, dim=1)
         p2_ = F.softmax(p2, dim=1)
         p3_ = F.softmax(p3, dim=1)
-        
+
         X1 = torch.sum(p1_.unsqueeze(1) * X1, dim=2)
         X2 = torch.sum(p2_.unsqueeze(1) * X2, dim=2)
         X3 = torch.sum(p3_.unsqueeze(1) * X3, dim=2)
-        
-        z = self.out_z(torch.cat([X1, X2, X3], dim=1))
+
+        z = self.out_z(
+            F.dropout(
+                F.relu(torch.cat([X1, X2, X3], dim=1)),
+                p=self.config.dropout,
+                training=self.training,
+            )
+        )
         return p1, p2, z

src/utils/model.py
@@ -54,29 +54,33 @@ def loss_model1(p1, p2, y1, y2, z, impossibles, **kwargs):
 
 
 def loss_model2(p1, p2, y1, y2, z, impossibles, **kwargs):
-    device = kwargs["device"]
     sa_max, _ = torch.max(p1, dim=1)
     sb_max, _ = torch.max(p2, dim=1)
-    sa, sb = p1 - sa_max.unsqueeze(1), p2 - sb_max.unsqueeze(1)
+    sa_max = sa_max.view(-1, 1)
+    sb_max = sb_max.view(-1, 1)
+
+    max_, _ = torch.max(torch.cat([sa_max, sb_max, z], dim=1), dim=1)
+    sa, sb, z = p1 - max_.unsqueeze(1), p2 - max_.unsqueeze(1), z - max_.unsqueeze(1)
 
     exp_sa, exp_sb, exp_z = torch.exp(sa), torch.exp(sb), torch.exp(z)
     normalizer = exp_z + (torch.sum(exp_sa, dim=1) * torch.sum(exp_sb, dim=1)).view(
         -1, 1
     )
-    exp_sa, exp_sb, exp_z = exp_sa / normalizer, exp_sb / normalizer, exp_z / normalizer
+    # exp_sa, exp_sb, exp_z = exp_sa / normalizer, exp_sb / normalizer, exp_z / normalizer
 
     N = p1.shape[0]
-    loss = torch.tensor(0.0).to(device)
+    loss = torch.tensor(0.0).to(p1.device)
     for i in range(N):
-        exp_sa_, exp_sb_, exp_z_ = exp_sa[i], exp_sb[i], exp_z[i, 0]
+        sa_, sb_, z_, norm_ = (sa[i], sb[i], z[i, 0], normalizer[i, 0])
         y1_, y2_ = y1[i], y2[i]
+
         if y1_ >= 400 or y2_ >= 400:
             # ignore loss calculation
             continue
         elif impossibles[i] == 0:
-            loss += -torch.log(exp_sa_[y1_] * exp_sb_[y2_])
+            loss += -sa_[y1_] - sb_[y2_] + torch.log(norm_)
         else:
-            loss += -torch.log(exp_z_)
+            loss += -z_ + torch.log(norm_)
 
     return loss / N
 
@@ -142,19 +146,30 @@ def pred_model1(p1, p2, z):
 def pred_model2(p1, p2, z):
     sa_max, _ = torch.max(p1, dim=1)
     sb_max, _ = torch.max(p2, dim=1)
-    sa, sb = p1 - sa_max.unsqueeze(1), p2 - sb_max.unsqueeze(1)
+    sa_max = sa_max.view(-1, 1)
+    sb_max = sb_max.view(-1, 1)
+
+    max_, _ = torch.max(torch.cat([sa_max, sb_max, z], dim=1), dim=1)
+
+    sa, sb, z = p1 - max_.unsqueeze(1), p2 - max_.unsqueeze(1), z - max_.unsqueeze(1)
 
     exp_sa, exp_sb, exp_z = torch.exp(sa), torch.exp(sb), torch.exp(z)
     normalizer = exp_z + (torch.sum(exp_sa, dim=1) * torch.sum(exp_sb, dim=1)).view(
         -1, 1
     )
-    exp_sa, exp_sb, exp_z = exp_sa / normalizer, exp_sb / normalizer, exp_z / normalizer
+    # exp_sa, exp_sb, exp_z = exp_sa / normalizer, exp_sb / normalizer, exp_z / normalizer
 
     N = p1.shape[0]
     ymin, ymax = [], []
 
     for i in range(N):
-        exp_sa_, exp_sb_, exp_z_ = exp_sa[i], exp_sb[i], exp_z[i, 0]
+        exp_sa_, exp_sb_, exp_z_, norm_ = (
+            exp_sa[i],
+            exp_sb[i],
+            exp_z[i, 0],
+            normalizer[i, 0],
+        )
+        # Note : we are not dividing by norm, divide by norm_ to get the normalized prob
         outer = torch.matmul(exp_sa_.unsqueeze(1), exp_sb_.unsqueeze(0))
         outer = torch.triu(outer)
         a1, _ = torch.max(outer, dim=1)